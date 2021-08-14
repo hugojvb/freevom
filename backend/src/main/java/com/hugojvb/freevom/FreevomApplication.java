00250a0033c0986b61e99c167d0b0639f78f1756
Hugo Batista <hugojvb20@gmail.com>
2021-08-14 13:47:24 +0100

    First commit

---
+++ backend/src/main/java/com/hugojvb/freevom/FreevomApplication.java
@@ -0,0 +1,13 @@
+package com.hugojvb.freevom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreevomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreevomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hugojvb/freevom/FreevomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugojvb.freevom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreevomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
