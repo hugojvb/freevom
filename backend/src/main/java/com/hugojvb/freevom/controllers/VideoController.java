355e77c9ad9515e8fd32ffe7482149a5c01fd420
Hugo Batista <hugojvb20@gmail.com>
2021-08-15 20:30:47 +0100

    Added repository save

--- backend/src/main/java/com/hugojvb/freevom/controllers/VideoController.java
+++ backend/src/main/java/com/hugojvb/freevom/controllers/VideoController.java
@@ -1,5 +1,6 @@
 package com.hugojvb.freevom.controllers;
 
+import com.hugojvb.freevom.services.VideoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/video")
+@RequiredArgsConstructor
 public class VideoController {
+
+    private final VideoService videoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void uploadVideo(@RequestParam("file") MultipartFile file) {
-
+        videoService.uploadVideo(file);
     }
     
 }
--- backend/src/main/java/com/hugojvb/freevom/services/VideoService.java
+++ backend/src/main/java/com/hugojvb/freevom/services/VideoService.java
@@ -1,9 +1,9 @@
 package com.hugojvb.freevom.services;
 
 import com.hugojvb.freevom.models.Video;
+import com.hugojvb.freevom.repositories.VideoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -11,10 +11,13 @@ import lombok.RequiredArgsConstructor;
 public class VideoService {
     
     private final S3Service s3Service;
+    private final VideoRepository videoRepository;
 
     public void uploadVideo(MultipartFile file) {
         String videoUrl = s3Service.uploadFile(file);
         var video = new Video();
         video.setVideoUrl(videoUrl);
+
+        videoRepository.save(video);
     }
 }
