84e3726a3af73646737714f452e8e62eb25481e1
Hugo Batista <hugojvb20@gmail.com>
2021-08-15 18:54:08 +0100

    Changed s3 service

--- backend/src/main/java/com/hugojvb/freevom/services/FileService.java
+++ backend/src/main/java/com/hugojvb/freevom/services/FileService.java
@@ -4,5 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     
-    String uploadFile(MultipartFile multipartFile);
+    String uploadFile(MultipartFile file);
 }
