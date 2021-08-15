89de87bf742a2e64c74d0e92cd7ecf6eca9da7d1
Hugo Batista <hugojvb20@gmail.com>
2021-08-15 15:44:57 +0100

    Added comment model

--- backend/src/main/java/com/hugojvb/freevom/models/Comment.java
+++ backend/src/main/java/com/hugojvb/freevom/models/Comment.java
@@ -1,5 +1,12 @@
 package com.hugojvb.freevom.models;
 
+import org.springframework.data.annotation.Id;
+
 public class Comment {
 
+    @Id
+    private String id;
+    private String text;
+    private String authorId;
+    private Integer likeCount;
 }
