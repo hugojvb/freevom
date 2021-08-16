4b352d865c2b0bf0de73641813d54e6be82ae7a2
Hugo Batista <hugojvb20@gmail.com>
2021-08-16 14:58:17 +0100

    Added react drag and drop simple

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import Routing from "./Routing"
+import React from "react";
+import "./App.css";
+import Routing from "./Routing";
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
-	return (
-		<>
-			<Routing/>
-		</>
-	);
+  return (
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <Routing />
+      </DndProvider>
+    </>
+  );
 }
 
 export default App;
--- frontend/client/src/components/Upload.tsx
+++ frontend/client/src/components/Upload.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import { useDrop } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
 
 const Upload = () => {
-    return (
-        <>
-        <h1>Upload</h1> 
-        </>
-    )
-}
+  const [droppedFiles, setDroppedFiles] = useState<File[]>([]);
 
-export default Upload
+  // HANDLE DROP
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: [NativeTypes.FILE],
+    drop(item: { files: any[] }) {
+      setDroppedFiles(item.files);
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+
+  const isActive = canDrop && isOver;
+
+  return (
+    <>
+      <div ref={drop}>{isActive ? "Release to drop" : "Drag a file here"}</div>
+      <div>
+        {droppedFiles.map((file) => (
+          <li key={file.name}>{file.name}</li>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Upload;
